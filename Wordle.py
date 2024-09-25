e1493dee01994827f441394cfd3646055cfd5aa5
acneu <acneu@willamette.edu>
2024-09-24 18:27:28 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name:
-# Collaborators (if any):
-# GenAI Transcript (if any):
-# Estimated time spent (hr):
+# Name: Anika Neu
+# Collaborators (if any): Quad Center
+# GenAI Transcript (if any): N/A
+# Estimated time spent (hr): 2
 # Description of any added extensions:
 ########################################
 
@@ -14,15 +14,19 @@ def wordle():
     # The main function to play the Wordle game.
 
     def enter_action():
+        row_number = gw.get_current_row()
+        string = "" #empty string to add letters to it
+        for i in range(N_COLS): #creating a loop
+            letter = gw.get_square_letter(row_number, i) #adding letters each word
+            string += letter
+        if is_english_word(string) and len(str(string)) == 5: #making sure that there is an input of 5 letters AND is an english word
+            gw.show_message("That's a 5 letter English word!") #
+        else:
+            gw.show_message("Not in word list, sorry!") #if the word entered is not in the word list
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
-
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
-
-
-
 
 # Startup boilerplate
 if __name__ == "__main__":
