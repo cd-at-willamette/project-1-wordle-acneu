c08153e0d2a7b92c0da613e7c1cf9a91a751bfa1
acneu <acneu@willamette.edu>
2024-09-29 16:17:11 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -2,7 +2,7 @@
 # Name: Anika Neu
 # Collaborators (if any): Quad Center
 # GenAI Transcript (if any): N/A
-# Estimated time spent (hr): 9
+# Estimated time spent (hr): 2
 # Description of any added extensions:
 ########################################
 
@@ -13,8 +13,6 @@ import random
 def wordle():
     # The main function to play the Wordle game.
 
-
-#milestone 1
     def enter_action():
         row_number = gw.get_current_row()
         string = "" #empty string to add letters to it
@@ -23,24 +21,21 @@ def wordle():
             string += letter
         if is_english_word(string) and len(str(string)) == 5: #making sure that there is an input of 5 letters AND is an english word
             gw.show_message("That's a 5 letter English word!") #
+            def color_guess(guess, correct):
+                letters_left = guess
+                for i in range(len(guess)):
+                    if guess[i] == correct[i]:
+                        gw.set_square_color(row_number, i, CORRECT_COLOR)
+                    elif guess[i] in correct:
+                        gw.set_square_color(row_number, i, PRESENT_COLOR)
+                    else:
+                        gw.set_square_color(row_number, i, MISSING_COLOR)
+            color_guess(string.lower(), correct_word)
+            color_keys(string.lower(), correct_word)
+            change_rows(string.lower())
         else:
             gw.show_message("Not in word list, sorry!") #if the word entered is not in the word list
-        # What should happen when RETURN/ENTER is pressed.
 
-#milestone 2
-        def color_guess(guess, correct):
-            letters_left = guess
-            gw.set_square_color(0,0, CORRECT_COLOR)
-            for i in range(len(guess)):
-                if guess[i] == correct[i]:
-                    gw.set_square_color(row_number, i, CORRECT_COLOR)
-                elif guess[i] in correct:
-                    gw.set_square_color(row_number, i, PRESENT_COLOR)
-                else:
-                     gw.set_square_color(row_number, i, MISSING_COLOR)
-        color_guess(string.lower(), correct_word)
-
-#milestone 3
     def find_random_word():
         random_word = ""
         while len(random_word) != 5:
@@ -50,8 +45,29 @@ def wordle():
     correct_word = find_random_word()
     print(correct_word)
 
+    def change_rows(string):
+        if correct_word != string:
+            current = gw.get_current_row()
+            if current != N_ROWS - 1:
+                current = current + 1
+                gw.set_current_row(current)
+            elif current == N_ROWS - 1:
+                gw.show_message("Sorry, you lose")
+        else:
+            gw.show_message("You win!")
 
-            
+    def color_keys(guess, correct):
+        for i in range(N_COLS):
+            letter = guess[i]
+            color = gw.get_square_color(gw.get_current_row(), i)
+            if color == CORRECT_COLOR:
+                gw.set_key_color(letter, CORRECT_COLOR)
+            elif color == PRESENT_COLOR:
+                if gw.get_key_color(letter) != CORRECT_COLOR:
+                    gw.set_key_color(letter, PRESENT_COLOR)
+            else:
+                if gw.get_key_color(letter) == UNKNOWN_COLOR:
+                    gw.set_key_color(letter, MISSING_COLOR
 
 
     gw = WordleGWindow()
